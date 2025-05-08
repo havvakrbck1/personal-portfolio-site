7d0fe3448fee39b7d39dd434bf26e3c1ef59d453
havvakrbck1 <geva44882@gmail.com>
2025-05-08 12:11:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-// Public klasöründeki dosyaları sunmak için:
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ana sayfa route'u (index.html'i gönderiyoruz)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
